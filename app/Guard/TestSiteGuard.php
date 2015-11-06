65a8798cd6a20d66c18707fae44960d139798ec5
Dan James Britchford <danbritchford@gmail.com>
2015-11-06 17:29:18 +0000

    New extension test of the guard

--- app/Guard/TestSiteGuard.php
+++ app/Guard/TestSiteGuard.php
@@ -5,7 +5,7 @@ namespace App\Guard;
 class TestSiteGuard extends \DJB\Guard\Guard {
 	
 	protected function canFly() {
-		if ( ! $this->value === "can fly") $this->setIssue('can not fly');
+		if ($this->value !== "can fly") $this->setIssue('can not fly');
 	}
 
 }
